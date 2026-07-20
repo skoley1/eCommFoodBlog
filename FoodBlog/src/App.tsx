86ec5ef60c99244e626a1e13bac1634f7272bafa
Arijit Pal <arijpal8@in.ibm.com>
2026-07-20 20:43:04 +0530

    Add GitHub Pages deployment: vite base path, HashRouter, Actions workflow

--- FoodBlog/src/App.tsx
+++ FoodBlog/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
